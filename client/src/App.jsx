2901ed730c846ac2380e0925fc1b636dad04e6e0
badal <mishrabadal988@gmail.com>
2026-01-18 07:50:36 +0530

    project basic design made and git initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favourite from "./pages/Favourite";
+import { Toaster,toast } from "react-hot-toast";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin")
+  return (
+    <>
+     <Toaster position="top-right" />
+    {/* {toast.success("Login successful")} */}
+       {!isAdminRoute && <Navbar />}
+       
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="my-bookings" element={<MyBookings />}></Route>
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+        {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
